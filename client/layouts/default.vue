f77bc9101c332aee1460fd86921c5417823b5e95
johndevofficial <info.johnlim@gmail.com>
2022-02-24 08:41:22 +0800

    updated .env.example and removed Navbar

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,17 +1,8 @@
 <template>
   <v-app app>
-    <Navbar/>
     <v-main>
       <nuxt />
     </v-main>
   </v-app>
 </template>
 <script>
-import Navbar from '~/components/global/Navbar.vue';
-
-export default {
-  components: {
-    Navbar
-  }
-}
-</script>
